0cb5ed23c63387afc6019d848cf55a56c1f19d5f
Pervaiz Iqbal <piqbal325@gmail.com>
2024-04-17 22:53:31 +0500

    First Commit

---
+++ src/componant/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Link from "next/link"
+function Footer(){
+    return(
+        <div>
+            This is Footer layout.
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/componant/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Link from "next/link"
+function Header(){
+    return(
+        <div>
+            This is Header layout.
+        </div>
+    )
+}
+
+export default Header
